5abcda7da818fd5301eb1b39e7f9878c6a222fb4
coolxitech <admin@sorpack.cn>
2025-04-17 14:30:44 +0800

    refactor(app): 重构 Index 控制器
    - 移除了不必要的 hello 方法
    - 修改了 index 方法的返回内容，使用 JSON 响应替代 iframe
    - 引入了 Response 工具类用于生成统一响应格式

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -3,16 +3,12 @@
 namespace app\controller;
 
 use app\BaseController;
+use app\utils\Response;
 
 class Index extends BaseController
 {
     public function index()
     {
-        return '<style>*{ padding: 0; margin: 0; }</style><iframe src="https://www.thinkphp.cn/welcome?version=' . \think\facade\App::version() . '" width="100%" height="100%" frameborder="0" scrolling="auto"></iframe>';
-    }
-
-    public function hello($name = 'ThinkPHP8')
-    {
-        return 'hello,' . $name;
+        return Response::json(0, '欢迎使用三角洲行动API');
     }
 }
