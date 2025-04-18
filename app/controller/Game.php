3a6fe3039b28fd22902a5701df06c220864c7ff6
coolxitech <admin@sorpack.cn>
2025-04-18 11:12:13 +0800

    feat(Game): 增加游戏相关数据接口
    - 添加烽火地带和全面战场战绩数据
    - 增加游戏配置信息获取
    - 支持指定赛季的玩家赛季信息获取
    - 新增登录流水、道具流水、哈夫币流水信息
    - 增加玩家资产数据，包括三角劵、三角币和哈夫币数量

--- app/controller/Game.php
+++ app/controller/Game.php
@@ -6,6 +6,7 @@ use app\utils\Response;
 use GuzzleHttp\Client;
 use GuzzleHttp\Cookie\CookieJar;
 use think\facade\Request;
+use think\response\Json;
 
 class Game
 {
@@ -19,12 +20,16 @@ class Game
         ]);
     }
 
-    public function getData()
+    public function getData(): Json
     {
         $openId = Request::param('openid');
         $accessToken = Request::param('access_token');
+        $seasonId = Request::param('seasonid'); // 可查指定赛季
         if (empty($openId) || empty($accessToken)) {
             return Response::json(-1, '参数错误');
+        }
+        if (empty($seasonId) || $seasonId == '') {
+            $seasonId = 3;
         }
         $gameData = [];
         $cookie = CookieJar::fromArray([
@@ -34,7 +39,7 @@ class Game
             'appid' => 101491592,
         ], '.qq.com');
 
-        // 战绩列表
+        // 烽火地带战绩
         $response = $this->client->request('POST', 'https://comm.ams.game.qq.com/ide/', [
             'form_params' => [
                 'iChartId' => 319386,
@@ -47,13 +52,48 @@ class Game
         ]);
         $result = $response->getBody()->getContents();
         $data = json_decode($result, true);
-        $gameData['matchList'] = $data['ret'] != 0 ? [] : $data['jData']['data'];
+        $gameData['matchList']['touchGold'] = $data['ret'] != 0 ? [] : $data['jData']['data'];
+
+        // 全面战场战绩
+        $response = $this->client->request('POST', 'https://comm.ams.game.qq.com/ide/', [
+            'form_params' => [
+                'iChartId' => 319386,
+                'iSubChartId' => 319386,
+                'sIdeToken' => 'zMemOt',
+                'type' => 5,
+                'page' => 1,
+            ],
+            'cookies' => $cookie,
+        ]);
+        $result = $response->getBody()->getContents();
+        $data = json_decode($result, true);
+        $gameData['matchList']['battlefield'] = $data['ret'] != 0 ? [] : $data['jData']['data'];
+
+        // 游戏配置信息
+        $response = $this->client->request('POST', 'https://comm.ams.game.qq.com/ide/', [
+            'form_params' => [
+                'iChartId' => 316968,
+                'iSubChartId' => 316968,
+                'sIdeToken' => 'KfXJwH',
+                'source' => 5,
+                'method' => 'dfm/config.list',
+                'param' => json_encode([
+                    'configType' => 'all',
+                ]),
+            ],
+            'cookies' => $cookie,
+        ]);
+        $result = $response->getBody()->getContents();
+        $data = json_decode($result, true);
+        $gameData['config'] = $data['ret'] != 0 ? [] : $data['jData']['data']['data']['config'];
+
+        // 玩家赛季信息
         $response = $this->client->request('POST', 'https://comm.ams.game.qq.com/ide/', [
             'form_params' => [
                 'iChartId' => 317814,
                 'iSubChartId' => 317814,
                 'sIdeToken' => 'QIRBwm',
-                'seasonid' => 3, // 赛季
+                'seasonid' => $seasonId,
             ],
             'cookies' => $cookie,
         ]);
@@ -67,6 +107,142 @@ class Game
                 'userData' => $data['jData']['userData'],
                 'careerData' => $data['jData']['careerData'],
             ];
+        }
+
+        // 登录流水信息
+        $response = $this->client->request('POST', 'https://comm.ams.game.qq.com/ide/', [
+            'form_params' => [
+                'iChartId' => 319386,
+                'iSubChartId' => 319386,
+                'sIdeToken' => 'zMemOt',
+                'type' => 1,
+            ],
+            'cookies' => $cookie,
+        ]);
+        $result = $response->getBody()->getContents();
+        $data = json_decode($result, true);
+        if ($data['ret'] != 0) {
+            $gameData['PlayerInfo']['login'] = [];
+        } else {
+            $gameData['PlayerInfo']['login'] = $data['jData']['data'];
+        }
+
+        // 道具流水信息
+        $response = $this->client->request('POST', 'https://comm.ams.game.qq.com/ide/', [
+            'form_params' => [
+                'iChartId' => 319386,
+                'iSubChartId' => 319386,
+                'sIdeToken' => 'zMemOt',
+                'type' => 2,
+            ],
+            'cookies' => $cookie,
+        ]);
+        $result = $response->getBody()->getContents();
+        $data = json_decode($result, true);
+        if ($data['ret'] != 0) {
+            $gameData['PlayerInfo']['item'] = [];
+        } else {
+            $gameData['PlayerInfo']['item'] = $data['jData']['data'];
+        }
+
+        // 哈夫币流水信息
+        $response = $this->client->request('POST', 'https://comm.ams.game.qq.com/ide/', [
+            'form_params' => [
+                'iChartId' => 319386,
+                'iSubChartId' => 319386,
+                'sIdeToken' => 'zMemOt',
+                'type' => 3,
+            ],
+            'cookies' => $cookie,
+        ]);
+        $result = $response->getBody()->getContents();
+        $data = json_decode($result, true);
+        if ($data['ret'] != 0) {
+            $gameData['PlayerInfo']['money'] = [];
+        } else {
+            $gameData['PlayerInfo']['money'] = $data['jData']['data'];
+            $gameData['PlayerInfo']['money']['total'] = $gameData['PlayerInfo']['money'][0]['totalMoney'];
+            unset($gameData['PlayerInfo']['money'][0]);
+        }
+
+        // 玩家资产
+        $response = $this->client->request('POST', 'https://comm.ams.game.qq.com/ide/', [
+            'form_params' => [
+                'iChartId' => 318948,
+                'iSubChartId' => 318948,
+                'sIdeToken' => 'Plaqzy',
+            ],
+            'cookies' => $cookie,
+        ]);
+        $result = $response->getBody()->getContents();
+        $data = json_decode($result, true);
+        if ($data['ret'] != 0) {
+            $gameData['PlayerAssets'] = [
+                'userData' => [],
+                'weponData' => [],
+                'dCData' => [],
+            ];
+        } else {
+            $gameData['PlayerAssets']['userData'] = $data['jData']['userData'];
+            $gameData['PlayerAssets']['weponData'] = $data['jData']['weponData'];
+            $gameData['PlayerAssets']['dCData'] = $data['jData']['dCData'][0];
+        }
+
+        // 三角劵数量
+        $response = $this->client->request('POST', 'https://comm.ams.game.qq.com/ide/', [
+            'form_params' => [
+                'iChartId' => 319386,
+                'iSubChartId' => 319386,
+                'sIdeToken' => 'zMemOt',
+                'type' => 3,
+                'item' => 17888808888,
+            ],
+            'cookies' => $cookie,
+        ]);
+        $result = $response->getBody()->getContents();
+        $data = json_decode($result, true);
+        if ($data['ret'] != 0) {
+            $gameData['PlayerAssets']['coin'] = 0;
+        } else {
+            $gameData['PlayerAssets']['coin'] = $data['jData']['data'][0]['totalMoney'];
+        }
+
+        // 三角币数量
+        $response = $this->client->request('POST', 'https://comm.ams.game.qq.com/ide/', [
+            'form_params' => [
+                'iChartId' => 319386,
+                'iSubChartId' => 319386,
+                'sIdeToken' => 'zMemOt',
+                'type' => 3,
+                'item' => 17888808889,
+            ],
+            'cookies' => $cookie,
+        ]);
+        $result = $response->getBody()->getContents();
+        $data = json_decode($result, true);
+        if ($data['ret'] != 0) {
+            $gameData['PlayerAssets']['tickets'] = 0;
+        } else {
+            $gameData['PlayerAssets']['tickets'] = $data['jData']['data'][0]['totalMoney'];
+        }
+
+        //哈夫币数量
+        $response = $this->client->request('POST', 'https://comm.ams.game.qq.com/ide/', [
+            'form_params' => [
+                'iChartId' => 319386,
+                'iSubChartId' => 319386,
+                'sIdeToken' => 'zMemOt',
+                'type' => 3,
+                'item' => 17020000010,
+            ],
+            'cookies' => $cookie,
+        ]);
+        $result = $response->getBody()->getContents();
+        $data = json_decode($result, true);
+        if ($data['ret'] != 0) {
+            $gameData['PlayerAssets']['money'] = 0;
+        } else {
+            $gameData['PlayerAssets']['money'] = $data['jData']['data'][0]['totalMoney'];
         }
 
         return Response::json(0, '获取成功', $gameData);
