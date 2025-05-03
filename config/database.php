f9795d90badc9f8a7f1b05e09165be68f3f0bf0a
coolxitech <admin@sorpack.cn>
2025-05-04 00:59:39 +0800

    feat(config): 添加 SQLite 数据库配置并设置为默认
    - 在数据库配置中添加了 SQLite 连接信息
    - 将默认数据库驱动从 MySQL 改为 SQLite
    - 保留了原有的 MySQL 配置，以便于切换

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 
 return [
     // 默认使用的数据库连接配置
-    'default'         => env('DB_DRIVER', 'mysql'),
+    'default'         => env('DB_DRIVER', 'sqlite'), // 默认为本地数据库，可选mysql
 
     // 自定义时间查询规则
     'time_query_rule' => [],
@@ -57,7 +57,11 @@ return [
             // 开启字段缓存
             'fields_cache'    => false,
         ],
-
+        'sqlite' => [
+            'type' => 'sqlite',
+            'dsn' => 'sqlite:./df.db',
+            'prefix' => 'df_',
+        ],
         // 更多的数据库配置信息
     ],
 ];
