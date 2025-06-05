c438d06b8771d3526dc91b5f45dfff5e8f901f57
coolxitech <admin@sorpack.cn>
2025-06-05 20:59:16 +0800

    feat(config): 添加弹药配置并补充配件位
    - 新增ammo.php文件，包含各种口径子弹的配置信息
    - 在accessory.php中添加新的配件位
    - 为accessory.php文件添加注释说明

---
+++ config/ammo.php
@@ -0,0 +1,170 @@
+<?php
+
+return [
+    'ammo5.56x45' => [
+        [
+            'name' => '5.56x45mm M995',
+            'grade' => 5,
+        ],
+        [
+            'name' => '5.56x45mm M855A1',
+            'grade' => 4,
+        ],
+        [
+            'name' => '5.56x45mm M855',
+            'grade' => 3,
+        ],
+        [
+            'name' => '5.56x45mm FMJ',
+            'grade' => 2,
+        ],
+        [
+            'name' => '5.56x45mm RRLP',
+            'grade' => 1,
+        ]
+    ],
+    'ammo7.62x39' => [
+        [
+            'name' => '7.62x39mm AP',
+            'grade' => 5,
+        ],
+        [
+            'name' => '7.62x39mm BP',
+            'grade' => 4,
+        ],
+        [
+            'name' => '7.62x39mm PS',
+            'grade' => 3,
+        ],
+        [
+            'name' => '7.62x39mm T45M',
+            'grade' => 2,
+        ],
+        [
+            'name' => '7.62x39mm LP',
+            'grade' => 1,
+        ]
+    ],
+    'ammo5.8x42' => [
+        [
+            'name' => '5.8x42mm DVC12',
+            'grade' => 5,
+        ],
+        [
+            'name' => '5.8x42mm DBP10',
+            'grade' => 4,
+        ],
+        [
+            'name' => '5.8x42mm DVP88',
+            'grade' => 3,
+        ],
+        [
+            'name' => '5.8x42mm DBP87',
+            'grade' => 2,
+        ],
+    ],
+    'ammo5.45x39' => [
+        [
+            'name' => '5.45x39mm BS',
+            'grade' => 5,
+        ],
+        [
+            'name' => '5.45x39mm BT',
+            'grade' => 4,
+        ],
+        [
+            'name' => '5.45x39mm PS',
+            'grade' => 3,
+        ],
+        [
+            'name' => '5.45x39mm T',
+            'grade' => 2,
+        ],
+        [
+           'name' => '5.45x39mm PRS',
+            'grade' => 1,
+        ],
+    ],
+    'ammo12.7x55' => [
+        [
+            'name' => '12.7x55mm PS12B',
+            'grade' => 5,
+        ],
+        [
+            'name' => '12.7x55mm PS12',
+            'grade' => 4,
+        ],
+        [
+            'name' => '12.7x55mm PD12双头弹',
+            'grade' => 4,
+        ],
+        [
+            'name' => '12.7x55mm PS12A',
+            'grade' => 3,
+        ],
+    ],
+    'ammo6.8x51' => [
+        [
+            'name' => '6.8x51mm AP',
+            'grade' => 6,
+        ],
+        [
+            'name' => '6.8x51mm Hybrid',
+            'grade' => 5,
+        ],
+        [
+            'name' => '6.8x51mm FMJ',
+            'grade' => 4,
+        ],
+    ],
+    'ammo7.62x51' => [
+        [
+            'name' => '7.62x51mm M61',
+            'grade' => 6,
+        ],
+        [
+            'name' => '7.62x51mm M62',
+            'grade' => 5,
+        ],
+        [
+            'name' => '7.62x51mm M80',
+            'grade' => 4,
+        ],
+        [
+            'name' => '7.62x51mm BPZ',
+            'grade' => 3,
+        ],
+        [
+            'name' => '7.62x51mm UN',
+            'grade' => 2,
+        ],
+    ],
+    'ammo9x39' => [
+        [
+            'name' => '9x39mm BP',
+            'grade' => 5,
+        ],
+        [
+            'name' => '9x39mm SP6',
+            'grade' => 4,
+        ],
+        [
+            'name' => '9x39mm SP5',
+            'grade' => 3,
+        ],
+    ],
+    'ammo7.62x35' => [
+        [
+            'name' => '.300BLK',
+            'grade' => 5,
+        ],
+        [
+            'name' => '.300BLK',
+            'grade' => 4,
+        ],
+        [
+            'name' => '.300BLK',
+            'grade' => 3,
+        ],
+    ],
+];
