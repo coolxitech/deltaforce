25c54107d74f5a824fa93e61f152427af64728c5
coolxitech <admin@sorpack.cn>
2025-07-14 10:13:11 +0800

    feat(controller): 添加 Steam 控制器
    - 新增 Steam 类，实现查询 Steam 封禁信息的功能(需要自行获取相关cookie)

---
+++ app/controller/Steam.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\controller;
+
+use app\utils\Response;
+use GuzzleHttp\Client;
+use GuzzleHttp\Cookie\CookieJar;
+use think\facade\Request;
+
+class Steam
+{
+    public function index()
+    {
+        $token = Request::param('token'); // 需要Steam的指定Coooie字符串(steamLoginSecure)
+        $client = new Client();
+        $response = $client->request('GET', 'https://help.steampowered.com/zh-cn/wizard/VacBans', [
+            'cookies' => CookieJar::fromArray([
+                'steamLoginSecure' => $token,
+            ], 'help.steampowered.com'),
+        ]);
+        $result = $response->getBody()->getContents();
+        preg_match_all('/help_highlight_text">(.*?)</', $result, $matches);
+        $games = $matches[1];
+        array_pop($games); // 删掉多余的提示字符串
+        return Response::json(0, '成功', $games);
+    }
+}
