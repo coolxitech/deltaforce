51a9c352423381d98899f00a5f9305377b844f15
coolxitech <admin@kuxi.tech>
2026-04-12 01:28:48 +0800

    feat(common): 添加二维码token生成函数
    - 实现getQrToken函数用于生成二维码token
    - 使用哈希算法处理qrSig字符串
    - 支持32位整数范围内的hash值计算
    - 添加了位运算优化处理逻辑

--- app/common.php
+++ app/common.php
@@ -5,3 +5,14 @@ function getMicroTime(): int
 {
     return round(microtime(true) * 1000);
 }
+
+function getQrToken(string $qrSig): int
+{
+    $len = strlen($qrSig);
+    $hash = 0;
+    for ($i = 0; $i < $len; $i++) {
+        $hash += (($hash << 5) & 2147483647) + ord($qrSig[$i]) & 2147483647;
+        $hash &= 2147483647;
+    }
+    return $hash & 2147483647;
+}
