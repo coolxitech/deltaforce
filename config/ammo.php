1a43d521d2328cf743f3b4a75ded6d45cc736026
coolxitech <admin@kuxi.tech>
2026-04-26 16:12:14 +0800

    feat(config): 添加弹药配置文件
    - 新增 .338 Lap Mag AP 弹药配置
    - 新增 .357 Magnum 多种弹药类型配置
    - 新增 .45 ACP 系列弹药配置
    - 新增 45-70 Govt 系列弹药配置
    - 新增 .50 AE 系列弹药配置
    - 新增 12 Gauge 霰弹系列配置
    - 新增 12.7x55mm 弹药配置
    - 新增 4.6x30mm 弹药配置
    - 新增 5.45x39mm 弹药配置
    - 新增 5.56x45mm 弹药配置
    - 新增 5.7x28mm 弹药配置
    - 新增 5.8x42mm 弹药配置
    - 新增 6.8x51mm 弹药配置
    - 新增 .300BLK 弹药配置
    - 新增 7.62x39mm 弹药配置
    - 新增 7.62x51mm 弹药配置
    - 新增 7.62x54R 弹药配置
    - 新增 9x19mm 弹药配置
    - 新增 9x39mm 弹药配置
    - 新增弓箭弹药配置

--- config/ammo.php
+++ config/ammo.php
@@ -1,344 +1,520 @@
 <?php
 
 return [
-    'ammo5.56x45' => [
+    'ammo.338' => [
         [
-            'name' => '5.56x45mm M995',
-            'grade' => 5,
-        ],
-        [
-            'name' => '5.56x45mm M855A1',
-            'grade' => 4,
-        ],
-        [
-            'name' => '5.56x45mm M855',
-            'grade' => 3,
-        ],
-        [
-            'name' => '5.56x45mm FMJ',
-            'grade' => 2,
-        ],
-        [
-            'name' => '5.56x45mm RRLP',
-            'grade' => 1,
-        ]
-    ],
-    'ammo7.62x39' => [
-        [
-            'name' => '7.62x39mm AP',
-            'grade' => 5,
-        ],
-        [
-            'name' => '7.62x39mm BP',
-            'grade' => 4,
-        ],
-        [
-            'name' => '7.62x39mm PS',
-            'grade' => 3,
-        ],
-        [
-            'name' => '7.62x39mm T45M',
-            'grade' => 2,
-        ],
-        [
-            'name' => '7.62x39mm LP',
-            'grade' => 1,
-        ]
-    ],
-    'ammo5.8x42' => [
-        [
-            'name' => '5.8x42mm DVC12',
-            'grade' => 5,
-        ],
-        [
-            'name' => '5.8x42mm DBP10',
-            'grade' => 4,
-        ],
-        [
-            'name' => '5.8x42mm DVP88',
-            'grade' => 3,
-        ],
-        [
-            'name' => '5.8x42mm DBP87',
-            'grade' => 2,
-        ],
-    ],
-    'ammo5.45x39' => [
-        [
-            'name' => '5.45x39mm BS',
-            'grade' => 5,
-        ],
-        [
-            'name' => '5.45x39mm BT',
-            'grade' => 4,
-        ],
-        [
-            'name' => '5.45x39mm PS',
-            'grade' => 3,
-        ],
-        [
-            'name' => '5.45x39mm T',
-            'grade' => 2,
-        ],
-        [
-           'name' => '5.45x39mm PRS',
-            'grade' => 1,
-        ],
-    ],
-    'ammo12.7x55' => [
-        [
-            'name' => '12.7x55mm PS12B',
-            'grade' => 5,
-        ],
-        [
-            'name' => '12.7x55mm PS12',
-            'grade' => 4,
-        ],
-        [
-            'name' => '12.7x55mm PD12双头弹',
-            'grade' => 4,
-        ],
-        [
-            'name' => '12.7x55mm PS12A',
-            'grade' => 3,
-        ],
-    ],
-    'ammo6.8x51' => [
-        [
-            'name' => '6.8x51mm AP',
+            'objectID' => 37240700001,
+            'name' => '.338 Lap Mag AP',
             'grade' => 6,
-        ],
-        [
-            'name' => '6.8x51mm Hybrid',
-            'grade' => 5,
-        ],
-        [
-            'name' => '6.8x51mm FMJ',
-            'grade' => 4,
-        ],
-    ],
-    'ammo7.62x51' => [
-        [
-            'name' => '7.62x51mm M61',
-            'grade' => 6,
-        ],
-        [
-            'name' => '7.62x51mm M62',
-            'grade' => 5,
-        ],
-        [
-            'name' => '7.62x51mm M80',
-            'grade' => 4,
-        ],
-        [
-            'name' => '7.62x51mm BPZ',
-            'grade' => 3,
-        ],
-        [
-            'name' => '7.62x51mm UN',
-            'grade' => 2,
-        ],
-    ],
-    'ammo9x39' => [
-        [
-            'name' => '9x39mm BP',
-            'grade' => 5,
-        ],
-        [
-            'name' => '9x39mm SP6',
-            'grade' => 4,
-        ],
-        [
-            'name' => '9x39mm SP5',
-            'grade' => 3,
-        ],
-    ],
-    'ammo7.62x35' => [
-        [
-            'name' => '.300BLK',
-            'grade' => 5,
-        ],
-        [
-            'name' => '.300BLK',
-            'grade' => 4,
-        ],
-        [
-            'name' => '.300BLK',
-            'grade' => 3,
-        ],
-    ],
-    'ammo9x19' => [
-        [
-            'name' => '9x19mm PBP',
-            'grade' => 4,
-        ],
-        [
-            'name' => '9x19mm RIP',
-            'grade' => 3,
-        ],
-        [
-            'name' => '9x19mm AP6.3',
-            'grade' => 3,
-        ],
-        [
-            'name' => '9x19mm Pst',
-            'grade' => 2,
-        ],
-        [
-            'name' => '9x19mm PSO',
-            'grade' => 1,
-        ],
-    ],
-    'ammo5.7x28' => [
-        [
-            'name' => '5.7x28mm SS190',
-            'grade' => 5,
-        ],
-        [
-            'name' => '5.7x28mm SS193',
-            'grade' => 4,
-        ],
-        [
-            'name' => '5.7x28mm R37.F',
-            'grade' => 3,
-        ],
-        [
-            'name' => '5.7x28mm L191',
-            'grade' => 3,
-        ],
-        [
-            'name' => '5.7x28mm SS197SR',
-            'grade' => 2,
-        ],
-        [
-            'name' => '5.7x28mm SS198LF',
-            'grade' => 1,
-        ],
-    ],
-    'ammo.45' => [
-        [
-            'name' => '.45 ACP AP',
-            'grade' => 4,
-        ],
-        [
-            'name' => '.45 ACP RIP',
-            'grade' => 3,
-        ],
-        [
-            'name' => '.45 ACP FMJ',
-            'grade' => 3,
-        ],
-        [
-            'name' => '.45 ACP JHP',
-            'grade' => 2,
-        ],
-        [
-            'name' => '.45 ACP HS',
-            'grade' => 1,
-        ],
-    ],
-    'ammo4.6x30' => [
-        [
-            'name' => '4.6x30mm AP SX',
-            'grade' => 5,
-        ],
-        [
-            'name' => '4.6x30mm FMJ SX',
-            'grade' => 4,
-        ],
-        [
-            'name' => '4.6x30mm Subsonic SX',
-            'grade' => 3,
         ],
     ],
     'ammo.357' => [
         [
-            'name' => '.357 Magnum FMJ',
-            'grade' => 4,
-        ],
-        [
+            'objectID' => 37220000001,
             'name' => '.357 Magnum 9号霰射"鼠弹"',
             'grade' => 3,
         ],
         [
+            'objectID' => 37220200001,
+            'name' => '.357 Magnum HP',
+            'grade' => 2,
+        ],
+        [
+            'objectID' => 37220300001,
             'name' => '.357 Magnum JHP',
             'grade' => 3,
         ],
         [
-            'name' => '.357 Magnum HP',
+            'objectID' => 37220400001,
+            'name' => '.357 Magnum FMJ',
+            'grade' => 4,
+        ],
+    ],
+    'ammo.45' => [
+        [
+            'objectID' => 37190000001,
+            'name' => '.45 ACP RIP',
+            'grade' => 3,
+        ],
+        [
+            'objectID' => 37190100001,
+            'name' => '.45 ACP HS',
+            'grade' => 1,
+        ],
+        [
+            'objectID' => 37190200001,
+            'name' => '.45 ACP JHP',
             'grade' => 2,
+        ],
+        [
+            'objectID' => 37190300001,
+            'name' => '.45 ACP FMJ',
+            'grade' => 3,
+        ],
+        [
+            'objectID' => 37190400001,
+            'name' => '.45 ACP AP',
+            'grade' => 4,
+        ],
+        [
+            'objectID' => 37190500001,
+            'name' => '.45 ACP Super​',
+            'grade' => 5,
+        ],
+    ],
+    'ammo.45-70' => [
+        [
+            'objectID' => 37290300001,
+            'name' => '45-70 Govt RN',
+            'grade' => 3,
+        ],
+        [
+            'objectID' => 37290400001,
+            'name' => '45-70 Govt FMJ',
+            'grade' => 4,
+        ],
+        [
+            'objectID' => 37290500001,
+            'name' => '45-70 Govt FTX',
+            'grade' => 5,
         ],
     ],
     'ammo.50' => [
         [
-            'name' => '.50 AE FMJ',
-            'grade' => 4,
+            'objectID' => 37230200001,
+            'name' => '.50 AE HP',
+            'grade' => 2,
         ],
         [
+            'objectID' => 37230300001,
             'name' => '.50 AE JHP',
             'grade' => 3,
         ],
         [
-            'name' => '.50 AE HP',
-            'grade' => 2,
-        ],
-    ],
-    'ammo7.62x54' => [
-        [
-            'name' => '7.62x54R SNB',
-            'grade' => 6,
-        ],
-        [
-            'name' => '7.62x54R BT',
-            'grade' => 5,
-        ],
-        [
-            'name' => '7.62x54R LPS',
+            'objectID' => 37230400001,
+            'name' => '.50 AE FMJ',
             'grade' => 4,
         ],
         [
-            'name' => '7.62x54R T46M',
-            'grade' => 3,
-        ],
-    ],
-    'ammo.338' => [
-        [
-            'name' => '.338 Lap Mag AP',
-            'grade' => 7,
+            'objectID' => 37230500001,
+            'name' => '.50 AE AP',
+            'grade' => 5,
         ],
     ],
     'ammo12' => [
         [
-            'name' => '12 Gauge独头 AP-20',
-            'grade' => 4,
-        ],
-        [
-            'name' => '12 Gauge 龙息弹',
-            'grade' => 4,
-        ],
-        [
+            'objectID' => 37250000001,
             'name' => '12 Gauge 8.5毫米鹿弹',
             'grade' => 3,
         ],
         [
-            'name' => '12 Gauge 箭形弹',
-            'grade' => 3,
-        ],
-        [
-            'name' => '12 Gauge独头 FTX',
-            'grade' => 3,
-        ],
-        [
+            'objectID' => 37250000002,
             'name' => '12 Gauge独头 RIP',
             'grade' => 2,
         ],
         [
+            'objectID' => 37250100001,
+            'name' => '12 Gauge 7毫米鹿弹',
+            'grade' => 1,
+        ],
+        [
+            'objectID' => 37250200002,
             'name' => '12 Gauge独头 GT',
             'grade' => 2,
         ],
         [
-            'name' => '12 Gauge 7毫米鹿弹',
+            'objectID' => 37250300001,
+            'name' => '12 Gauge 箭形弹',
+            'grade' => 3,
+        ],
+        [
+            'objectID' => 37250300002,
+            'name' => '12 Gauge独头 FTX',
+            'grade' => 3,
+        ],
+        [
+            'objectID' => 37250400002,
+            'name' => '12 Gauge独头 AP-20',
+            'grade' => 4,
+        ],
+        [
+            'objectID' => 37250400003,
+            'name' => '12 Gauge 龙息弹',
+            'grade' => 4,
+        ],
+        [
+            'objectID' => 37250400004,
+            'name' => '12 Gauge 独头 APX',
+            'grade' => 4,
+        ],
+    ],
+    'ammo12.7x55' => [
+        [
+            'objectID' => 37160300001,
+            'name' => '12.7x55mm PS12A',
+            'grade' => 3,
+        ],
+        [
+            'objectID' => 37160400001,
+            'name' => '12.7x55mm PS12',
+            'grade' => 4,
+        ],
+        [
+            'objectID' => 37160400002,
+            'name' => '12.7x55mm PD12双头弹',
+            'grade' => 4,
+        ],
+        [
+            'objectID' => 37160500001,
+            'name' => '12.7x55mm PS12B',
+            'grade' => 5,
+        ],
+    ],
+    'ammo4.6x30' => [
+        [
+            'objectID' => 37260300001,
+            'name' => '4.6x30mm Subsonic SX',
+            'grade' => 3,
+        ],
+        [
+            'objectID' => 37260400001,
+            'name' => '4.6x30mm FMJ SX',
+            'grade' => 4,
+        ],
+        [
+            'objectID' => 37260500001,
+            'name' => '4.6x30mm AP SX',
+            'grade' => 5,
+        ],
+    ],
+    'ammo5.45x39' => [
+        [
+            'objectID' => 37120100001,
+            'name' => '5.45x39mm PRS',
             'grade' => 1,
+        ],
+        [
+            'objectID' => 37120200001,
+            'name' => '5.45x39mm T',
+            'grade' => 2,
+        ],
+        [
+            'objectID' => 37120300001,
+            'name' => '5.45x39mm PS',
+            'grade' => 3,
+        ],
+        [
+            'objectID' => 37120400001,
+            'name' => '5.45x39mm BT',
+            'grade' => 4,
+        ],
+        [
+            'objectID' => 37120400002,
+            'name' => '5.45x39mm BT +P',
+            'grade' => 4,
+        ],
+        [
+            'objectID' => 37120400003,
+            'name' => '5.45*39mm BT ST',
+            'grade' => 4,
+        ],
+        [
+            'objectID' => 37120500001,
+            'name' => '5.45x39mm BS',
+            'grade' => 5,
+        ],
+        [
+            'objectID' => 37120500003,
+            'name' => '5.45*39mm BS ST+',
+            'grade' => 5,
+        ],
+    ],
+    'ammo5.56x45' => [
+        [
+            'objectID' => 37100100001,
+            'name' => '5.56x45mm RRLP',
+            'grade' => 1,
+        ],
+        [
+            'objectID' => 37100200001,
+            'name' => '5.56x45mm FMJ',
+            'grade' => 2,
+        ],
+        [
+            'objectID' => 37100300001,
+            'name' => '5.56x45mm M855',
+            'grade' => 3,
+        ],
+        [
+            'objectID' => 37100300002,
+            'name' => '5.56*45mm M855 APC',
+            'grade' => 3,
+        ],
+        [
+            'objectID' => 37100400001,
+            'name' => '5.56x45mm M855A1',
+            'grade' => 4,
+        ],
+        [
+            'objectID' => 37100400002,
+            'name' => '5.56*45mm M855A1 APC+',
+            'grade' => 4,
+        ],
+        [
+            'objectID' => 37100500001,
+            'name' => '5.56x45mm M995',
+            'grade' => 5,
+        ],
+    ],
+    'ammo5.7x28' => [
+        [
+            'objectID' => 37210000001,
+            'name' => '5.7x28mm R37.F',
+            'grade' => 3,
+        ],
+        [
+            'objectID' => 37210100001,
+            'name' => '5.7x28mm SS198LF',
+            'grade' => 1,
+        ],
+        [
+            'objectID' => 37210200001,
+            'name' => '5.7x28mm SS197SR',
+            'grade' => 2,
+        ],
+        [
+            'objectID' => 37210300001,
+            'name' => '5.7x28mm L191',
+            'grade' => 3,
+        ],
+        [
+            'objectID' => 37210400001,
+            'name' => '5.7x28mm SS193',
+            'grade' => 4,
+        ],
+        [
+            'objectID' => 37210500001,
+            'name' => '5.7x28mm SS190',
+            'grade' => 5,
+        ],
+    ],
+    'ammo5.8x42' => [
+        [
+            'objectID' => 37130200001,
+            'name' => '5.8x42mm DBP87',
+            'grade' => 2,
+        ],
+        [
+            'objectID' => 37130300001,
+            'name' => '5.8x42mm DVP88',
+            'grade' => 3,
+        ],
+        [
+            'objectID' => 37130400001,
+            'name' => '5.8x42mm DBP10',
+            'grade' => 4,
+        ],
+        [
+            'objectID' => 37130500001,
+            'name' => '5.8x42mm DVC12',
+            'grade' => 5,
+        ],
+    ],
+    'ammo6.8x51' => [
+        [
+            'objectID' => 37150400001,
+            'name' => '6.8x51mm FMJ',
+            'grade' => 4,
+        ],
+        [
+            'objectID' => 37150500001,
+            'name' => '6.8x51mm Hybrid',
+            'grade' => 5,
+        ],
+        [
+            'objectID' => 37150600001,
+            'name' => '6.8x51mm AP',
+            'grade' => 6,
+        ],
+    ],
+    'ammo7.62x35' => [
+        [
+            'objectID' => 37280300001,
+            'name' => '.300BLK',
+            'grade' => 3,
+        ],
+        [
+            'objectID' => 37280300002,
+            'name' => '.300BLK V SUB',
+            'grade' => 3,
+        ],
+        [
+            'objectID' => 37280400001,
+            'name' => '.300BLK',
+            'grade' => 4,
+        ],
+        [
+            'objectID' => 37280400002,
+            'name' => '.300BLK BCP SUB',
+            'grade' => 4,
+        ],
+        [
+            'objectID' => 37280500001,
+            'name' => '.300BLK',
+            'grade' => 5,
+        ],
+    ],
+    'ammo7.62x39' => [
+        [
+            'objectID' => 37110100001,
+            'name' => '7.62x39mm LP',
+            'grade' => 1,
+        ],
+        [
+            'objectID' => 37110200001,
+            'name' => '7.62x39mm T45M',
+            'grade' => 2,
+        ],
+        [
+            'objectID' => 37110300001,
+            'name' => '7.62x39mm PS',
+            'grade' => 3,
+        ],
+        [
+            'objectID' => 37110400001,
+            'name' => '7.62x39mm BP',
+            'grade' => 4,
+        ],
+        [
+            'objectID' => 37110400002,
+            'name' => '7.62*39mm BP SUB',
+            'grade' => 4,
+        ],
+        [
+            'objectID' => 37110500001,
+            'name' => '7.62x39mm AP',
+            'grade' => 5,
+        ],
+        [
+            'objectID' => 37110500002,
+            'name' => '7.62*39mm AP SUB',
+            'grade' => 5,
+        ],
+    ],
+    'ammo7.62x51' => [
+        [
+            'objectID' => 37170200001,
+            'name' => '7.62x51mm UN',
+            'grade' => 2,
+        ],
+        [
+            'objectID' => 37170300001,
+            'name' => '7.62x51mm BPZ',
+            'grade' => 3,
+        ],
+        [
+            'objectID' => 37170400001,
+            'name' => '7.62x51mm M80',
+            'grade' => 4,
+        ],
+        [
+            'objectID' => 37170500001,
+            'name' => '7.62x51mm M62',
+            'grade' => 5,
+        ],
+        [
+            'objectID' => 37170600001,
+            'name' => '7.62x51mm M61',
+            'grade' => 6,
+        ],
+    ],
+    'ammo7.62x54' => [
+        [
+            'objectID' => 37180300001,
+            'name' => '7.62x54R T46M',
+            'grade' => 3,
+        ],
+        [
+            'objectID' => 37180400001,
+            'name' => '7.62x54R LPS',
+            'grade' => 4,
+        ],
+        [
+            'objectID' => 37180500001,
+            'name' => '7.62x54R BT',
+            'grade' => 5,
+        ],
+        [
+            'objectID' => 37180600001,
+            'name' => '7.62x54R SNB',
+            'grade' => 6,
+        ],
+    ],
+    'ammo9x19' => [
+        [
+            'objectID' => 37200000001,
+            'name' => '9x19mm RIP',
+            'grade' => 3,
+        ],
+        [
+            'objectID' => 37200100001,
+            'name' => '9x19mm PSO',
+            'grade' => 1,
+        ],
+        [
+            'objectID' => 37200200001,
+            'name' => '9x19mm Pst',
+            'grade' => 2,
+        ],
+        [
+            'objectID' => 37200300001,
+            'name' => '9x19mm AP6.3',
+            'grade' => 3,
+        ],
+        [
+            'objectID' => 37200400001,
+            'name' => '9x19mm PBP',
+            'grade' => 4,
+        ],
+    ],
+    'ammo9x39' => [
+        [
+            'objectID' => 37140300001,
+            'name' => '9x39mm SP5',
+            'grade' => 3,
+        ],
+        [
+            'objectID' => 37140400001,
+            'name' => '9x39mm SP6',
+            'grade' => 4,
+        ],
+        [
+            'objectID' => 37140500001,
+            'name' => '9x39mm BP',
+            'grade' => 5,
+        ],
+    ],
+    'ammoArrow' => [
+        [
+            'objectID' => 37270300001,
+            'name' => '玻纤柳叶箭矢',
+            'grade' => 3,
+        ],
+        [
+            'objectID' => 37270400001,
+            'name' => '碳纤维刺骨箭矢',
+            'grade' => 4,
+        ],
+        [
+            'objectID' => 37270500001,
+            'name' => '碳纤维穿甲箭矢',
+            'grade' => 5,
+        ],
+        [
+            'objectID' => 37270500002,
+            'name' => '碳纤维散射箭矢',
+            'grade' => 5,
         ],
     ],
 ];
+
